3519a9902c8815be647b21329f2c392de328d3ba
sergkukuev <kukuev.serg@gmail.com>
2021-12-02 23:48:22 +0300

    Added reminder comparator

--- app/src/main/java/com/github/sergkukuev/anykeeper/Reminder.kt
+++ app/src/main/java/com/github/sergkukuev/anykeeper/Reminder.kt
@@ -5,3 +5,13 @@ data class Reminder(
     val lifetime: Long,
     val timestamp: Long
 )
+
+class ReminderComparator {
+    companion object : Comparator<Reminder> {
+        override fun compare(o1: Reminder, o2: Reminder): Int {
+            val t1 = o1.timestamp + o1.lifetime
+            val t2 = o2.timestamp + o2.lifetime
+            return (t1 - t2).toInt()
+        }
+    }
+}
